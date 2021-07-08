b41bfebeecd84c49bfcc1a04f6622345e29d565c
Anmolbaral <58747993+Anmolbaral@users.noreply.github.com>
2021-07-08 13:44:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import Navbar from './components/Navbar.js';
 
 
 Promise.all([
-      d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSa2-2dvTEoDZJzuCmvkHQwwO3gGQMfjbNgO56gU5KmgJsluaOXgkuRd2UgWsKXeVeSQLhlQ6yv86KZ/pub?output=csv"),
-      d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ9EXny1oBEoaY0kV0WOYWMJyByGx6uo76KsfnSZ413LNlNNr0uU_b86kHxyavIj_FCotd3Ir76rtHe/pub?output=csv")
+      d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTAJIIKnmqE-yA6HwtrzUU-9pueHRQM4pcjc5wqo4usmjHYaBO5DxQP6r9CQTJby8VrWl8Ct2VZNQg-/pub?output=csv"),
+      d3.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRNuI6q9-ynZe79UNxyOStdf2QiuIiT5DxbK7tbICPqdAETPRTGthEFS9ugujq930fREwg2X2bgBze3/pub?output=csv")
       ])
       .then(([about, projects]) => {
         const data = {about, projects};
