0b531520674a2bc1fd1370c0559dc41e6d4394ed
Milan <milandahal213@gmail.com>
2021-07-12 21:40:40 -0400

    Update About.js

--- components/About.js
+++ components/About.js
@@ -8,7 +8,9 @@ export default function About(about){
             <p>${about[0].description}</p>
             <div class="project-img">
             </div>
-            
+            <a href="${about[0].buttonlink}" target="_blank">
+                    <button class="button" style="margin-top: 30px; margin-bottom: 50px;">${about[0].buttonlabel}</button>
+                </a>
                 <a href="${about[0].buttonlink}" target="_blank">
                     <button class="button" style="margin-top: 30px; margin-bottom: 50px;">${about[0].buttonlabel}</button>
                 </a>
