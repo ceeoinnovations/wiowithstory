47110438df2960e1483b73863441767f0ba32531
Milan <milandahal213@gmail.com>
2021-07-13 08:54:33 -0400

    Update About.js

--- components/About.js
+++ components/About.js
@@ -8,6 +8,11 @@ export default function About(about){
             <p>${about[0].description}</p>
             <div class="project-img">
             </div>
+            
+            <form method="get" action="./hackathon.uf2">
+                    <button class="button" style="margin-top: 30px; margin-bottom: 50px;"> Firmware </button>
+</form>
+
             <a href="https://drive.google.com/drive/u/1/folders/1B2GM1fcwV51vLE_9PwgWTxeGHP0479U9" target="_blank">
                     <button class="button" style="margin-top: 30px; margin-bottom: 50px;"> Firmware </button>
                 </a>
